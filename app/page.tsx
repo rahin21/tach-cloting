e1ed4ee651449389e0ba5dd1e7f7289472f5ffc3
rahin21 <mdrahinzaman21@gmail.com>
2024-09-06 07:08:37 +0600

    home and footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Hello World</h1>
+      
     </main>
   );
 }
