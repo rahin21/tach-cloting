f31ad02dfe476cbf1b16f2462b46fe2af10f446d
rahin21 <mdrahinzaman21@gmail.com>
2024-09-15 10:26:31 +0600

    products

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,62 @@
-import Product from '@/components/shop/product'
-import React from 'react'
-
+import React from "react";
+import Product from "@/components/shop/product";
+const products = [
+  {
+    img: "/shop/shop1.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/shop/shop2.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/shop/shop3.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/home/dresses.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/home/hero.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/home/hero2.jpg",
+    name: "Plain T-shirt",
+    price: "$150.00",
+  },
+  {
+    img: "/home/kint.jpg",
+    name: "Knits",
+    price: "$150.00",
+  },
+  {
+    img: "/home/tops.jpg",
+    name: "Tops",
+    price: "$150.00",
+  },
+];
 function ShopPage() {
-	return (
-		<div>
-			<Product/>
-		</div>
-	)
+  return (
+    <div className="flex justify-center">
+			<div  className="grid md:grid-cols-2 gap-x-20 gap-y-10">
+      {products.map((product, i) => (
+          <Product
+					key={i} 
+            img={product.img}
+            name={product.name}
+            price={product.price}
+          />
+				))}
+        </div>
+    </div>
+  );
 }
 
-export default ShopPage
+export default ShopPage;
--- components/shop/product.tsx
+++ components/shop/product.tsx
@@ -2,21 +2,25 @@ import Image from "next/image";
 import React from "react";
 import shopImg from "/public/shop/shop3.jpg";
 
-function Product() {
+function Product({img, price, name}:{img:string, price:string, name:string}) {
   return (
-    <div className="relative group">
-      <div className="aspect-[3/4] w-[40] inline-block overflow-hidden items-end relative">
+    <div className="relative cursor-pointer">
+      <div className="aspect-[3/4] w-[27rem] inline-block overflow-hidden items-end relative group ">
         <Image
-          src={shopImg}
-          width={300}
-          height={300}
+          src={img}
+          width={400}
+          height={400}
           alt="img"
-          className=" w-full h-full object-cover hover:scale-110 transition-transform duration-300"
+          className="w-full h-full object-cover group-hover:scale-110 relative transition-transform duration-300"
         />
-      <div className="absolute bottom-0 left-0 w-full inset-0 bg-opacity-80 text-center bg-white text-black py-2">
-        <p >Quick View</p>
+        <div className="absolute -bottom-10 group-hover:bottom-0 left-0 w-full opacity-80 text-center transition-all duration-300 ">
+          <p className="bg-white text-black py-2">Quick View</p>
+        </div>
       </div>
-      </div>
+			<div className="font-medium">
+				<h1 className="text-xl">{name}</h1>
+				<p>{price}</p>
+			</div>
     </div>
   );
 }
